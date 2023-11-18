6886ac21415b7cbd6d37afd2c3c8c1aee74d89d3
Sebastian <saamayac@eafit.edu.co>
2023-11-18 22:54:53 +0000

    changes cap1

--- ANALISIS_proyecto/funciones_cap/cap1/Newton_LM.m
+++ ANALISIS_proyecto/funciones_cap/cap1/Newton_LM.m
@@ -1,5 +1,12 @@
 function [Punto_Inicial,tabla] = Newton_LM(Punto_Inicial,Tolerancia,Num_Max_Iteraciones,Funcion_Objetivo,Control_E)
 
+% setup
+if Control_E=="Absoluto"
+    func_error = @(x,x0) abs(x-x0);
+else
+    func_error = @(x,x0) abs((x-x0)/x);
+end
+
 syms x
 Derivada=diff(Funcion_Objetivo);
 Contador=0;
@@ -10,32 +17,26 @@ Derivada_Eval=Derivada_Eval_Tabla;
 Error_Tabla(Contador+1)=Tolerancia+1;
 Error=Error_Tabla(Contador+1);
 X_n_Tabla(Contador+1)=Punto_Inicial;
-if Control_E=="Absoluto"
-    while Error>Tolerancia && Contador<Num_Max_Iteraciones
-        X_n_Tabla(Contador+2)=Punto_Inicial-Funcion_Eval/Derivada_Eval;
-        Funcion_Eval_Tabla(Contador+2)=double(subs(Funcion_Objetivo,x,X_n_Tabla(Contador+2)));
-        Funcion_Eval=Funcion_Eval_Tabla(Contador+2);
-        Derivada_Eval_Tabla(Contador+2)=double(subs(Derivada,x,X_n_Tabla(Contador+2)));
-        Derivada_Eval=Derivada_Eval_Tabla(Contador+2);
-        Error_Tabla(Contador+2)=abs(X_n_Tabla(Contador+2)-Punto_Inicial);
-        Error=Error_Tabla(Contador+2);
-        Punto_Inicial=X_n_Tabla(Contador+2);
-        Contador=Contador+1;
-    end
-else
-    while Error>Tolerancia && Contador<Num_Max_Iteraciones
+
+while Error>Tolerancia && Contador<Num_Max_Iteraciones
+
     X_n_Tabla(Contador+2)=Punto_Inicial-Funcion_Eval/Derivada_Eval;
+
     Funcion_Eval_Tabla(Contador+2)=double(subs(Funcion_Objetivo,x,X_n_Tabla(Contador+2)));
     Funcion_Eval=Funcion_Eval_Tabla(Contador+2);
+
     Derivada_Eval_Tabla(Contador+2)=double(subs(Derivada,x,X_n_Tabla(Contador+2)));
     Derivada_Eval=Derivada_Eval_Tabla(Contador+2);
-    Error_Tabla(Contador+2)=abs((X_n_Tabla(Contador+2)-Punto_Inicial)/X_n_Tabla(Contador+2));
+
+    Error_Tabla(Contador+2) = func_error(X_n_Tabla(Contador+2), Punto_Inicial);
     Error=Error_Tabla(Contador+2);
-    Punto_Inicial=X_n_Tabla(Contador+2);
+
+    Punto_Inicial = X_n_Tabla(Contador+2);
     Contador=Contador+1;
 
-    end
 end
+
+
 if Funcion_Eval==0
     fprintf('%f es una aproximación de una raiz de f(x) con una tolerancia= %f \n',Punto_Inicial,Tolerancia)
 elseif Error<Tolerancia
